d280436e9df2990edb46231e88d60f160ca9d738
Prakash9019 <plsprakash2003@gmail.com>
2025-08-02 08:03:39 +0530

    added

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React,{useEffect} from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import ReachLinkPreview from './ReachLinkPreview'
+
+function App() {
+
+  return (
+    <div className="relative h-screen overflow-hidden">
+     <ReachLinkPreview />
+     </div>
+  )
+}
+
+export default App
